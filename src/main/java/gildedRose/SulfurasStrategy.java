f589f5a95bac41e73bf0edbead8f832de5ebd760
LIUJE7 <LIUJE7@oocl.cpm>
2019-08-19 20:27:05 +0800

    [Extract whenItemQualitySmallThanZero() in the SulfurStrangely class]

--- src/main/java/gildedRose/SulfurasStrategy.java
+++ src/main/java/gildedRose/SulfurasStrategy.java
@@ -3,13 +3,14 @@ package gildedRose;
 public class SulfurasStrategy extends ChangeStrategy {
     @Override
     void updateItem(Item item) {
+        whenItemQualitySmallThanZero(item);
+        if (item.sellIn < 0) {
+            whenItemQualitySmallThanZero(item);
+        }
+    }
+    private void whenItemQualitySmallThanZero(Item item) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
-        }
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
         }
     }
 }
