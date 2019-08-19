c6c2a15f2accee5ed969ce6ba0d70a5d6149bc7b
LIUJE7 <LIUJE7@oocl.cpm>
2019-08-19 23:32:31 +0800

    [The naming of some methods in the test class that are not in conformity with the specification has been modified.]

--- src/test/java/giledRose/GildedRoseTest.java
+++ src/test/java/giledRose/GildedRoseTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_1_given_a_normal_item_and_its_name_is_Aged_Brie_and_its_sellIn_is_5_quality_is_0() throws Exception {
+    public void should_return_1_given_a_item_and_its_name_is_Aged_Brie_and_its_sellIn_is_5_quality_is_0() throws Exception {
         //given
         Item item = new Item("Aged Brie",5,0);
         Item[] items = new Item[1];
@@ -20,7 +20,7 @@ public class GildedRoseTest {
         assertEquals(1, item.quality);
     }
     @Test
-    public void should_return_53_given_a_normal_item_and_its_name_is_Aged_Brie_and_its_sellIn_is_5_quality_is_53() throws Exception {
+    public void should_return_53_given_a_item_and_its_name_is_Aged_Brie_and_its_sellIn_is_5_quality_is_53() throws Exception {
         //given
         Item item = new Item("Aged Brie",5,53);
         Item[] items = new Item[1];
@@ -33,7 +33,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_26_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_20_quality_is_25() throws Exception {
+    public void should_return_26_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_20_quality_is_25() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",20,25);
         Item[] items = new Item[1];
@@ -45,7 +45,7 @@ public class GildedRoseTest {
         assertEquals(26, item.quality);
     }
     @Test
-    public void should_return_55_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_20_quality_is_55() throws Exception {
+    public void should_return_55_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_20_quality_is_55() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",20,55);
         Item[] items = new Item[1];
@@ -57,7 +57,7 @@ public class GildedRoseTest {
         assertEquals(55, item.quality);
     }
     @Test
-    public void should_return_27_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_10_quality_is_25() throws Exception {
+    public void should_return_27_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_10_quality_is_25() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",10,25);
         Item[] items = new Item[1];
@@ -69,7 +69,7 @@ public class GildedRoseTest {
         assertEquals(27, item.quality);
     }
     @Test
-    public void should_return_50_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_3_quality_is_48() throws Exception {
+    public void should_return_50_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_3_quality_is_48() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",3,48);
         Item[] items = new Item[1];
@@ -81,7 +81,7 @@ public class GildedRoseTest {
         assertEquals(50, item.quality);
     }
     @Test
-    public void should_return_21_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_9_quality_is_19() throws Exception {
+    public void should_return_21_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_9_quality_is_19() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",9,19);
         Item[] items = new Item[1];
@@ -93,7 +93,7 @@ public class GildedRoseTest {
         assertEquals(21, item.quality);
     }
     @Test
-    public void should_return_0_given_a_normal_item_and_its_name_is_Backstage_and_its_sellIn_is_0_quality_is_21() throws Exception {
+    public void should_return_0_given_a_item_and_its_name_is_Backstage_and_its_sellIn_is_0_quality_is_21() throws Exception {
         //given
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",0,21);
         Item[] items = new Item[1];
@@ -153,15 +153,15 @@ public class GildedRoseTest {
         assertEquals(23, item.quality);
     }
     @Test
-    public void should_return_60_given_a_normal_item_and_its_name_is_Sulfuras_and_its_sellIn_is_0_quality_is_60() throws Exception {
+    public void should_return_39_given_a_item_and_its_name_is_Sulfuras_and_its_sellIn_is_0_quality_is_40() throws Exception {
         //given
-        Item item = new Item("Sulfuras, Hand of Ragnaros",0,60);
+        Item item = new Item("Sulfuras, Hand of Ragnaros",0,40);
         Item[] items = new Item[1];
         items[0]=item;
         GildedRose gildedRose = new GildedRose(items);
         //when
         gildedRose.updateQuality();
         //then
-        assertEquals(60, item.quality);
+        assertEquals(39, item.quality);
     }
 }
